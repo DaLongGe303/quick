0b13379648a246f7014e20ecea5dcb6f4f6e6c4c
xlf <260226246@qq.com>
2019-04-11 21:59:20 +0800

    qucik version 2.0

---
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.baizhi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @RequestMapping("u")
+    @ResponseBody
+    public String select(){
+        
+        System.out.println("---------3333-----");
+        
+        return "00000000000000000";
+    }
+}
